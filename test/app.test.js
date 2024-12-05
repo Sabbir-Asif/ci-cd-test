edaf76e2204a6b642f8c9ebdfa2bd8e5769fac81
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-12-04 21:03:00 -0800

    second commit

--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,19 @@
 const request = require('supertest');
 const app = require('../app');
 
+let server; // Declare a server variable
+
+beforeAll(() => {
+  server = app.listen(4000); // Start the server on a different port
+});
+
+afterAll((done) => {
+  server.close(done); // Close the server after tests
+});
+
 describe('GET /', () => {
   it('should return Hello World!', async () => {
-    const res = await request(app).get('/');
+    const res = await request(server).get('/');
     expect(res.statusCode).toEqual(200);
     expect(res.text).toEqual('Hello World!');
   });
